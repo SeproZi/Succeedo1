8e0bb47e2c90931bdb3252d802b0ceb969af839c
Atilla Seprodi <atilla.seprodi@visma.com>
2025-08-05 12:30:34 +0000

    use npm?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // IMPORTANT: Replace this with your actual Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8EDVnJpB_C_D3m4y_T2Yx_Bw9p-ZkYxY",
-  authDomain: "succeedo-by-proceedo-a9a35.firebaseapp.com",
-  projectId: "succeedo-by-proceedo-a9a35",
-  storageBucket: "succeedo-by-proceedo-a9a35.appspot.com",
-  messagingSenderId: "53894101443",
-  appId: "1:53894101443:web:96e2888f8d9b15b130e527"
+  apiKey: "AIzaSyCV_MptESLcyN51o0Pj6PCgXzVqGqOykXg",
+  authDomain: "test-prcas-cm.firebaseapp.com",
+  projectId: "test-prcas-cm",
+  storageBucket: "test-prcas-cm.appspot.com",
+  messagingSenderId: "931253569530",
+  appId: "1:931253569530:web:9c7553f473c0993952f1e6"
 };
 
 // Initialize Firebase
