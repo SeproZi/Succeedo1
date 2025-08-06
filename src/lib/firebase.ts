d8d359542b7ed8559b9794672b4691c25bb8726c
Atilla Seprodi <atilla.seprodi@visma.com>
2025-08-06 10:48:38 +0000

    this is what I have in project settings at the web app I created in cons

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,14 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from your project settings
 const firebaseConfig = {
   apiKey: "AIzaSyDNTbT-SuOATom7t5e8c0ygZjMSiaf9aGo",
   authDomain: "test-prcas-cm.firebaseapp.com",
   projectId: "test-prcas-cm",
-  storageBucket: "test-prcas-cm.appspot.com",
+  storageBucket: "test-prcas-cm.firebasestorage.app",
   messagingSenderId: "926638638358",
   appId: "1:926638638358:web:b48e457a61780649f7bc37",
   measurementId: "G-LT26RP9GXH"
