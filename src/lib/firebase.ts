7eba511c42da7d29d81e3ef336919a8aa35bf451
Atilla Seprodi <atilla.seprodi@visma.com>
2025-08-06 12:44:39 +0000

    remember we have this set up already in firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration from your project settings
 const firebaseConfig = {
-  apiKey: "AIzaSyDNTbT-SuOATom7t5e8cOygZjMSiaf9aGo",
+  apiKey: "YOUR_API_KEY", // This is a placeholder, Firebase will replace it.
   authDomain: "test-prcas-cm.firebaseapp.com",
   projectId: "test-prcas-cm",
-  storageBucket: "test-prcas-cm.firebasestorage.app",
+  storageBucket: "test-prcas-cm.appspot.com",
   messagingSenderId: "926638638358",
-  appId: "1:926638638358:web:b48e457a61780649f7bc37",
-  measurementId: "G-LT26RP9GXH"
+  appId: "1:926638638358:web:b48e457a61780649f7bc37"
 };
 
 // Initialize Firebase
