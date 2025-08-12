f74da493eeb0a8968e49adee404c733021e13ef2
Atilla Seprodi <atilla.seprodi@visma.com>
2025-08-12 19:24:56 +0000

    refactor: Implement correct server/client pattern for dynamic routes

--- src/app/(okr)/department/new/page.tsx
+++ src/app/(okr)/department/new/page.tsx
@@ -1,16 +1,11 @@
 
+// SERVER FILE – OK to export route options here
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
 
-'use client';
-import dynamic from 'next/dynamic'
-
-const NewDepartmentClientPage = dynamic(
-  () => import('@/components/app/new-department-client-page'),
-  { ssr: false }
-)
+import NewDepartmentClientPage from '@/components/app/new-department-client-page';
 
 export default function Page() {
-  return <NewDepartmentClientPage />
+  return <NewDepartmentClientPage />;
 }
